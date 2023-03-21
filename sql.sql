8575ba104bc361c88eb5b829587e90fbbc1b0d2c
Tatinaa <tarajaonasitera@gmail.com>
2023-03-21 08:47:04 +0100

    ty vao tena izy

--- sql.sql
+++ sql.sql
@@ -2,8 +2,7 @@ CREATE DATABASE gestionCompta
 
 CREATE TABLE devise(
     idDevise SERIAL PRIMARY KEY,
-    libelle VARCHAR,
-    valeur DECIMAL
+    libelle VARCHAR
 );
 
 CREATE TABLE typeSaisie(
@@ -135,4 +134,56 @@ CREATE TABLE grandLivre(
     FOREIGN KEY (idCompteGeneral) REFERENCES compteGeneral(idCompteGeneral),
     FOREIGN KEY (idExercice) REFERENCES exercice(idExercice)
 );
+
+CREATE TABLE journal(
+    idJournal SERIAL PRIMARY KEY,
+    idSociete INT,
+    date DATE,
+    numeroPiece INT,
+    referencePiece VARCHAR,
+    numeroCompteGeneral INT, 
+    numeroCompteTiers INT,
+    libelle VARCHAR,
+    debit DECIMAL,
+    credit DECIMAL,
+    FOREIGN KEY (idSociete) REFERENCES societe(idSociete),
+    FOREIGN KEY (numeroCompteGeneral) REFERENCES compteGeneral(idCompteGeneral),
+    FOREIGN KEY (numeroCompteTiers) REFERENCES compteTiers(idCompteTiers)
+);
+
+CREATE TABLE valeurDevise(
+    idValeurDevise SERIAL PRIMARY KEY,
+    idDevise INT,
+    valeur DECIMAL,
+    date DATE,
+    FOREIGN KEY (idDevise) REFERENCES devise(idDevise)
+);
+
+INSERT INTO devise VALUES(1,'Ariary');
+INSERT INTO devise VALUES(1,'Dollard');
+INSERT INTO devise VALUES(1,'Euro');
+INSERT INTO devise VALUES(1,'Mur');
+INSERT INTO devise VALUES(1,'Yuan');
+
+-- INSERT INTO caracteristiqueCompta VALUES(1,)
+-- INSERT INTO societe VALUES();
+
+-- INSERT INTO compteGeneral VALUES(1,)
+
+
+INSERT INTO compteGeneral('numero','intitule') VALUES(41000,'Clients');
+INSERT INTO compteGeneral('numero','intitule') VALUES(40000,'Fournisseurs');
+INSERT INTO compteGeneral('numero','intitule') VALUES(51200,'Banque');
+INSERT INTO compteGeneral('numero','intitule') VALUES(40110,'Client locaux');
+INSERT INTO compteGeneral('numero','intitule') VALUES(40120,'Client etranger');
+INSERT INTO compteGeneral('numero','intitule') VALUES(40910,'Fournisseur:facture a recevoir');
+INSERT INTO compteGeneral('numero','intitule') VALUES(53000,'Caisse');
+INSERT INTO compteGeneral('numero','intitule') VALUES(60000,'Charges');
+INSERT INTO compteGeneral('numero','intitule') VALUES(70000,'Produits');
+INSERT INTO compteGeneral('numero','intitule') VALUES(20000,'Immobilisations');
+-- INSERT INTO compteGeneral('numero','intitule') VALUES(53000,'Caisse');
+-- INSERT INTO compteGeneral('numero','intitule') VALUES(53000,'Caisse');
+
+INSERT INTO compteTiers('idCompteGeneral','code','intitule') VALUES(,'AC')
+
 
